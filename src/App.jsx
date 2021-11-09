670b6034ddbbb378b0fc9a4c383f50d61392b7a3
Nan <40654091+01201617@users.noreply.github.com>
2021-11-09 16:09:33 +0900

    同様に完了と戻す機能を追加戻す機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,22 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncomplete = [...incompleteTodos];
+    const target = newIncomplete.pop(index);
+    const newComplete = [...completeTodos, target];
+    setIncompleteTodos(newIncomplete);
+    setCompleteTodos(newComplete);
+  };
+
+  const onClickBack = (index) => {
+    const newComplete = [...completeTodos];
+    const target = newComplete.pop(index);
+    const newIncomplete = [...incompleteTodos, target];
+    setCompleteTodos(newComplete);
+    setIncompleteTodos(newIncomplete);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -37,7 +53,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
@@ -47,11 +63,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
