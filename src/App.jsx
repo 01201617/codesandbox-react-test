2d4e97a6ae709cb8df6f5def173620f176d5f718
Nan <40654091+01201617@users.noreply.github.com>
2021-11-09 15:42:59 +0900

    削除機能追加(mapからindex取得しそれをbuttonに渡してspliceで削除)

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,16 @@ export const App = () => {
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   const onClickAdd = () => {
+    if (todoText === "") return;
     const newList = [...incompleteTodos, todoText];
     setIncompleteTodos(newList);
     setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
   };
 
   return (
@@ -26,12 +33,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
